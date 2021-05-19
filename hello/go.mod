e4212fcd228d62c84a35e0d63fe75f2e6ecf3e53
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-05-19 10:03:47 +0800

    hello,world

---
+++ hello/go.mod
@@ -0,0 +1,5 @@
+module github.com/Zheng7426/Golang-practice
+
+go 1.16
+
+require rsc.io/quote v1.5.2
---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	//fmt.Println("Hello, 世界")
+	fmt.Println(quote.Go())
+}
+
